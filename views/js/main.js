657acce2b128eb355f7b566ee44f2e14bc325420
민우 천 <cmw9101@konkuk.ac.kr>
2017-01-30 04:56:42 +0900

    83 is highest

--- views/js/main.js
+++ views/js/main.js
@@ -521,8 +521,9 @@ window.addEventListener('scroll', updatePositions);
 document.addEventListener('DOMContentLoaded', function() {
   var cols = 8;
   var s = 256;
+  var elem;
   for (var i = 0; i < 40; i++) {
-    var elem = document.createElement('img');
+    elem = document.createElement('img');
     elem.className = 'mover';
     elem.src = "images/pizzamini.png";
     elem.style.height = "100px";
